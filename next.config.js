9b31997d6fa89ee4d9ee1417a1764f29cf0bd36b
dbsrl9067 <dbsrl9067@gmail.com>
2026-04-06 22:51:16 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
 });
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+  // Turbopack 관련 경고를 무시하고 Webpack 설정을 허용함
+  images: {
+    unoptimized: true, // 미얀마 현지 저사양 기기를 위해 이미지 최적화 비활성화 (선택)
+  },
+};
+
+module.exports = withPWA(nextConfig);
